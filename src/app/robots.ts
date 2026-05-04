05e068d0c37997513ac05093b848fd8a2bb6c324
ngynphong <124499195+ngynphong@users.noreply.github.com>
2026-05-04 16:28:54 +0700

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://ocop.vn';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.ocop.iesconnect.vn';
 
   return {
     rules: {
